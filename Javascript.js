7e5d136dbaeaa64ca3154c6624054d3ac30bde15
willian <emai@xgames.com>
2025-03-03 16:13:30 -0800

    Adicionando arquivos

--- Javascript.js
+++ Javascript.js
@@ -1,37 +1,128 @@
+var caixa2 = document.getElementById("caixa2");
 
-gsap.registerPlugin(ScrollTrigger) 
+var course = ["HTML","CSS","JAVASCRIPT","NODE.JS","SASS"] 
 
+var ver = document.getElementById("btn3")
 
+var remover = document.getElementById("btn2")
 
-tl = gsap.timeline({
-
-    scrollTrigger:{
-
-
-        trigger:"section",
-        start:"top end",
-        end: "bottom top",
-        ease:1,
-        scrub: true,       
+var input = document.querySelector("#input")
 
 
+var antes = document.getElementById("btn4")
 
-    }
+var depois = document.getElementById("btn5")
 
+
+
+
+var tirar = ()=>{
+
+
+  var todos = [...document.querySelectorAll('.selec')]
+
+  todos.map((el)=>{
+
+    el.classList.remove("selec")
+
+
+  })
+
+}
+
+var createnewcourse = (curso)=>{
+
+
+  var div =  document.createElement("div")
+
+  div.setAttribute("class", "course")
+
+    div.innerHTML = curso;
+
+
+
+    div.addEventListener("click",(evt)=>{
+      tirar()
+      evt.target.classList.toggle("selec")
+
+    })
+    
+
+    caixa2.appendChild(div)
+
+    return div 
+
+}
+
+const selec = ()=>{
+
+
+  var todos = [...document.querySelectorAll('.selec')]
+
+
+//retorna somente todos que tem class todos ativo com return 
+    return todos[0]
+
+
+  
+
+
+}
+
+
+
+course.map((el)=>{
+
+  const novoelement0 = createnewcourse(el)
+
+  caixa2.appendChild(novoelement0)
 
 })
 
 
-tl.to("section",{
+remover.addEventListener("click",()=>{
 
-    x:900,
-    y:500,
+  var rs = selec()
+
+  rs.remove()
+
+})
+
+
+ver.addEventListener("click",()=>{
+
+var rs = selec();
+
+
+
+alert("O curso selecionado : "+ rs)
 
 
 })
 
-tl.to("section",{
+antes.addEventListener("click",()=>{
 
-    x:0,
+  var rs = selec();
+
+  
+
+
+  const novoelement00 = createnewcourse(input.value)
+
+  caixa2.insertBefore(novoelement00,rs)
+
+
+
+})
+depois.addEventListener("click",()=>{
+
+  var rs = selec();
+
+
+
+    const novoelement00 = createnewcourse(input.value)
+  
+
+    caixa2.insertBefore(novoelement00,rs.nextSibling)
 
 })
